8ae14d06d92588d5523b7f7ee8f5b0ae3be58e66
Allison Hurley <allison@hurleyhome.com>
2019-12-14 20:14:52 -0500

    General cleanup, especially volume control and parking time

--- MainControls.py
+++ MainControls.py
@@ -5,7 +5,15 @@ import RPi.GPIO as GPIO
 import time
 import datetime
 import alsaaudio
+import os
+import os.path
+from enum import Enum, auto
 
+class Mode(Enum):
+    Awake = auto()
+    Sleep = auto()
+    Snooze = auto()
+    
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.IN)         #Read output from PIR motion sensor
@@ -14,29 +22,48 @@ GPIO.setup(3, GPIO.OUT)         #LED output pin
 
 mixer = alsaaudio.Mixer('PCM')
 unsnooze = datetime.datetime.now()
+mode = Mode.Sleep
+off_file = "/home/pi/SmartAlarm/screenoff"
+if os.path.exists(off_file):
+    os.unlink(off_file)
+subprocess.run(["/home/pi/SmartAlarm/alarm_off"])
 
 while True:
     now = datetime.datetime.now()
+    motion_detected = GPIO.input(11) == 1
+    snooze_pressed = GPIO.input(13) == 1
 
-    i=GPIO.input(11)
+    if mode == Mode.Sleep:
+        # check to see if file screenoff has been deleted
+        if not os.path.exists(off_file):
+            mode = Mode.Awake
+    
+    elif mode == Mode.Awake:
 
-    if i==0: #When output from motion sensor is LOW
+        if motion_detected:
+            # go to sleep
+            subprocess.run(["/home/pi/SmartAlarm/alarm_off"])
+            mode = Mode.Sleep
 
-        # print "No intruders",i
+        elif snooze_pressed:
+            mode = Mode.Snooze
+            unsnooze = now + datetime.timedelta(minutes=1)
+            mixer.setvolume(0)
+            time.sleep(0.1)
+            mixer.setvolume(0)
+            time.sleep(0.1)
+            mixer.setvolume(0)
 
-        time.sleep(0.1)
+    elif mode == Mode.Snooze:
 
-    elif i==1: #When output from motion sensor is HIGH
+        if motion_detected:
+            # go to sleep
+            subprocess.run(["/home/pi/SmartAlarm/alarm_off"])
+            mode = Mode.Sleep
 
-        subprocess.run(["/home/pi/SmartAlarm/alarm_off"])
+        if now > unsnooze:
+            mixer.setvolume(100)
+            mode = Mode.Awake
 
-        time.sleep(0.1)
+    time.sleep(0.1)
 
-    if GPIO.input(13) == 1:
-        # snooze active
-        mixer.setvolume(0)
-        unsnooze = now + datetime.timedelta(minutes=1)
-
-    if now >= unsnooze:
-        mixer.setvolume(100)
-        
