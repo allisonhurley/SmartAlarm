24a0e6c8a2339f23f6af43d8441463e084ec8d12
Shayna <shayna.sande@gmail.com>
2019-12-18 01:50:57 -0500

    with formatting for html

--- FlaskUI/flaskui.py
+++ FlaskUI/flaskui.py
@@ -5,6 +5,7 @@ import json
 app = Flask(__name__)
 config_data = { "snooze": 9, "max-active": 120 }
 with open('/home/pi/SmartAlarm/config.json') as json_file:
+#with open('C:/Users/Shayna/Desktop/Smart_Alarm/SmartAlarm/config.json') as json_file:
     config_data = json.load(json_file)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -17,11 +18,13 @@ def index():
 @app.route('/on', methods=['GET', 'POST'])
 def on():
     subprocess.run(["/home/pi/SmartAlarm/alarm_on"])
+    #subprocess.run(["C:/Users/Shayna/Desktop/Smart_Alarm/SmartAlarm/alarm_on"])
     return redirect("/")
 
 @app.route('/off', methods=['GET', 'POST'])
 def off():
     subprocess.run(["/home/pi/SmartAlarm/alarm_off"])
+    #subprocess.run(["C:/Users/Shayna/Desktop/Smart_Alarm/SmartAlarm/alarm_off"])
     return redirect("/")
 
 @app.route('/config', methods=['GET', 'POST'])
@@ -32,8 +35,10 @@ def config():
             # save form values
             config_data = request.form
             with open("/home/pi/SmartAlarm/config.json", "w") as out:
+            #with open("C:/Users/Shayna/Desktop/Smart_Alarm/SmartAlarm/config.json", "w") as out:
                 out.write(json.dumps(request.form))
             subprocess.run(["/home/pi/SmartAlarm/adjust_cron"])
+            #subprocess.run(["C:/Users/Shayna/Desktop/Smart_Alarm/SmartAlarm/adjust_cron"])
         return redirect("/")
     return render_template("config.html", config_data=config_data)
 
